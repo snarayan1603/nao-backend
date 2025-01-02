6a2fec734d8a7cff2140c482304aa36ef275a052
Narayan Singh <ghost@ghost.local>
2025-01-02 11:37:03 +0530

    ssl added

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# main.py
 import os
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from transformers import MarianTokenizer, MarianMTModel
@@ -23,14 +21,12 @@ class TranslationRequest(BaseModel):
 
 @app.post("/api/translate")
 async def translate_text(req: TranslationRequest):
-    # Dynamically construct the model name based on the source and target language codes
     model_name = f"Helsinki-NLP/opus-mt-{req.source_lang}-{req.target_lang}"
     print(f"{model_name=}")
     try:
         tokenizer = MarianTokenizer.from_pretrained(model_name)
         model = MarianMTModel.from_pretrained(model_name)
     except Exception as e:
-        # If the model doesn't exist for the provided language pair, return a 400 error
         raise HTTPException(
             status_code=400,
             detail=f"Language pair '{req.source_lang}-{req.target_lang}' not supported or model not found."
@@ -42,9 +38,6 @@ async def translate_text(req: TranslationRequest):
 
     return {"original": req.text, "translated": translated_text}
 
-
 @app.get("/api/verify")
 async def verify():
-  
-
     return {"status": "Working"}
